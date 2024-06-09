fb3e96d12ba70f904a55fedaa5bbc935c1164ac6
jhonashc <jhonhuiracocha123456789@gmail.com>
2024-06-09 18:35:30 -0500

    drivers module updated: new driver events added

--- src/drivers/enums/drivers.enum.ts
+++ src/drivers/enums/drivers.enum.ts
@@ -1,4 +1,8 @@
 export enum DriverEvent {
   JOIN_ROUTE = 'join-route',
   LEAVE_ROUTE = 'leave-route',
+  START_ROUTE = 'start-route',
+  ROUTE_STARTED = 'route-started',
+  LOCATION_UPDATE = 'location-update',
+  BROADCAST_LOCATION = 'broadcast-location',
 }
